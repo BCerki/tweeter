d37c89edc5f363103ee52825fff61bb90be7d2d9
Brianna <briannac@shaw.ca>
2021-04-21 00:44:10 +0000

    create createTweetElement function and temp test code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,9 +4,51 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//function to display tweet time//
 $(document).ready(function () {
   timeago.render(document.querySelectorAll(".time-ago-formatted"));
   // timeago.format($(".time-ago-formatted").html());
   // $("time-ago-formatted").timeago();
 
 });
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+
+createTweetElement = function (tweetData) {
+  const $tweet = $(
+    `<article class="tweet">
+      <header>
+        <div>
+          <span><img src="${tweetData.user.avatars}">${tweetData.user.name}</span>
+        </div>
+        <div class="handle">${tweetData.user.handle}</div>
+      </header>
+      <div class="tweet-content">${tweetData.content.text}</div>
+      <footer>
+        <div class="time-ago-formatted">${tweetData.created_at}</div>
+        <div class="icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+      </article>`);
+    return $tweet;
+
+};
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
